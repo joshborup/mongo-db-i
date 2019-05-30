5d181486780f0187936c9da9176373bd66b02bcb
josh <joshborup@gmail.com>
2019-05-29 18:17:14 -0700

    getting staring point

--- server/controller/customerController.js
+++ server/controller/customerController.js
@@ -1,48 +1,14 @@
+// require the customer schema
 const Customer = require("../Schema/customer");
 
 module.exports = {
   getAllCustomers: (req, res) => {
-    Customer.find({}).then(customers => {
-      res.status(200).send(customers);
-    });
+    // implement get all customers logic
   },
   postCustomer: (req, res) => {
-    const { name, age, description } = req.body;
-    // create a new Customer with your Customer schema and pass in the correct values
-    // when creating data, we dont want to write to the DB directly, we want to run this through our validators
-    const customer = new Customer({
-      name,
-      age,
-      description
-    });
-
-    customer.save(customer => {
-      Customer.find({}).then(customers => {
-        res.status(200).send(customers);
-      });
-    });
+    // create a new Customer with your Customer schema and pass in the correct values from req.body
+    // when creating data, we dont want to write to the DB directly, we want to run this through our validators by makeing a new instance of the object and then saving it to the db
   },
-  updateCustomer: (req, res) => {
-    const { name, age } = req.query;
-
-    Customer.findOne({ name }).then(foundCustomer => {
-      foundCustomer.age = age;
-      foundCustomer.save(() => {
-        Customer.find({}).then(customers => {
-          res.status(200).send(customers);
-        });
-      });
-    });
-  },
-  deleteCustomer: (req, res) => {
-    const { name, age } = req.query;
-    // ok to delete things directly as we dont care about validators
-    Customer.deleteOne({ name }).then(customer => {
-      console.log("info about deletion ===>", customer);
-      // send all customers to the front
-      Customer.find({}).then(customers => {
-        res.status(200).send(customers);
-      });
-    });
-  }
+  updateCustomer: (req, res) => {},
+  deleteCustomer: (req, res) => {}
 };
--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
+// require mongoose
 const app = express();
 app.use(express.json());
+
 const {
   getAllCustomers,
   postCustomer,
@@ -13,14 +14,6 @@ const {
 const { SERVER_PORT, CONNECTION_STRING } = process.env;
 
 // make connection to mongo database
-mongoose
-  .connect(CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useCreateIndex: true
-  })
-  .then(() => {
-    console.log("mongo connected");
-  });
 
 app.get("/api/customer", getAllCustomers);
 
@@ -29,5 +22,6 @@ app.post("/api/customer", postCustomer);
 app.put("/api/customer", updateCustomer);
 
 app.delete("/api/customer", deleteCustomer);
+
 const port = SERVER_PORT || 4000;
 app.listen(port, () => console.log(`server up and running on ${port}`));
